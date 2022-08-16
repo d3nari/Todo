f5e05af93dae0b0522ca718b8a7fee9086976059
d3nari <dungerway@gmail.com>
2022-08-16 13:06:34 +0300

    Start working on adding dbContext in repo

---
+++ Todo/Todo/Application/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace Todo.Application
+{
+    public interface IUnitOfWork
+    {
+        public Task CommitAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Todo/Todo/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Todo.Application;
+
+namespace Todo.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TodoDbContext _dbContext;
+
+        public UnitOfWork(TodoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task CommitAsync(CancellationToken cancellationToken = default)
+        {
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
